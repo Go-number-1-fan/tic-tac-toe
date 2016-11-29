d7bbb0501af0fd26377ce1ead5728e5bc0a3d6af
Tom McGee <gonumber1fan@gmail.com>
2016-11-29 10:36:02 -0600

    Allow the user to play a basic Human Vs Human Tic Tac Toe without Status message
    * No Status Message displaying whose turn it is, etc.
    * Create the entire UI, and some mocks
    * Add helper functions to board for UI
    * Create entire game loop (never ends for now)
    * Move Board and Marker into Board package to not require importing Game all the time, and remove circular dependencies

--- player/player.go
+++ player/player.go
@@ -1,8 +1,8 @@
 package player
 
-import . "github.com/go-number-1-fan/tic-tac-toe/marker"
+import . "github.com/go-number-1-fan/tic-tac-toe/board"
 
 type Player interface {
-	GetMove() int
+	GetMove(board Board) int
 	GetMarker() Marker
 }
