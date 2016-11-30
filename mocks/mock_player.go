7a95dda21c0d9d8edaa46ffbc78e9e91daa2cf04
Tom McGee <gonumber1fan@gmail.com>
2016-11-30 09:05:16 -0600

    Pass UI through Game instead of creating a player with a UI

--- mocks/mock_player.go
+++ mocks/mock_player.go
@@ -1,13 +1,14 @@
 package mocks
 
 import . "github.com/go-number-1-fan/tic-tac-toe/board"
+import . "github.com/go-number-1-fan/tic-tac-toe/ui"
 
 type MockPlayer struct {
 	Marker Marker
 	Move   int
 }
 
-func (player MockPlayer) GetMove(board Board) int {
+func (player MockPlayer) GetMove(board Board, ui UI) int {
 	return player.Move
 }
 
--- player/human_player.go
+++ player/human_player.go
@@ -5,11 +5,10 @@ import . "github.com/go-number-1-fan/tic-tac-toe/ui"
 
 type HumanPlayer struct {
 	Marker Marker
-	UI     UI
 }
 
-func (player HumanPlayer) GetMove(board Board) int {
-	return player.UI.GetValidMove(board)
+func (player HumanPlayer) GetMove(board Board, ui UI) int {
+	return ui.GetValidMove(board)
 }
 
 func (player HumanPlayer) GetMarker() Marker {
