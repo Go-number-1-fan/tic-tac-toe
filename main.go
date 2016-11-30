d2e638f7552cb72f238407843f5d6c2d65c121e8
Tom McGee <gonumber1fan@gmail.com>
2016-11-30 16:55:45 -0600

    Adds Easy Computer Player that takes a random spot

--- main.go
+++ main.go
@@ -7,6 +7,6 @@ import . "github.com/go-number-1-fan/tic-tac-toe/player"
 
 func main() {
 	ui := ConsoleUI{ConsoleInput{}, ConsoleOutput{}}
-	game := CreateGame(ui, EmptyBoard(), HumanPlayer{"X"}, HumanPlayer{"O"}, StandardReferee{})
+	game := CreateGame(ui, EmptyBoard(), HumanPlayer{"X"}, ComputerPlayer{"O"}, StandardReferee{})
 	game.PlayGame()
 }
