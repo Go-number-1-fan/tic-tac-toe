8dcaf1d797512377c26335f8322603077e261f6d
Tom McGee <gonumber1fan@gmail.com>
2016-11-30 14:38:14 -0600

    Add Standard Referee, with only horizontal wins.

--- game/game_test.go
+++ game/game_test.go
@@ -7,17 +7,17 @@ import . "github.com/go-number-1-fan/tic-tac-toe/ui"
 import "testing"
 
 func TestGame_HasABoard(t *testing.T) {
-	assert.Equal(t, EmptyBoard(), Game{MockUI{0}, EmptyBoard(), []Player{MockPlayer{"X", 0}, MockPlayer{"O", 1}}}.board)
+	assert.Equal(t, EmptyBoard(), CreateGame(MockUI{0}, EmptyBoard(), MockPlayer{"X", 0}, MockPlayer{"O", 1}, MockReferee{Continue}).board)
 }
 
 func TestGame_CanMakeAMoveOnABoard(t *testing.T) {
-	game := Game{MockUI{0}, EmptyBoard(), []Player{MockPlayer{"X", 0}, MockPlayer{"O", 1}}}
+	game := CreateGame(MockUI{0}, EmptyBoard(), MockPlayer{"X", 0}, MockPlayer{"O", 1}, MockReferee{Continue})
 	game = game.takeTurn()
 	assert.NotEqual(t, EmptyBoard(), game.board)
 }
 
 func TestGame_CanMakeAMoveAtASelectedLocation(t *testing.T) {
-	game := Game{MockUI{0}, EmptyBoard(), []Player{MockPlayer{"X", 4}, MockPlayer{"O", 1}}}
+	game := CreateGame(MockUI{0}, EmptyBoard(), MockPlayer{"X", 4}, MockPlayer{"O", 1}, MockReferee{Continue})
 	game = game.takeTurn()
 	assert.Equal(t, X, game.board[4])
 }
@@ -25,15 +25,39 @@ func TestGame_CanMakeAMoveAtASelectedLocation(t *testing.T) {
 func TestGame_CanSwitchThePlayersAtTheEndOfTheTurn(t *testing.T) {
 	currentPlayer := MockPlayer{"X", 4}
 	nextPlayer := MockPlayer{"O", 1}
-	game := Game{MockUI{0}, EmptyBoard(), []Player{currentPlayer, nextPlayer}}
+	game := CreateGame(MockUI{0}, EmptyBoard(), currentPlayer, nextPlayer, MockReferee{Continue})
 	updatedGame := game.takeTurn()
 	assert.Equal(t, nextPlayer, updatedGame.players[0])
+}
+
+func TestGame_DoesNotSwitchThePlayersAtTheEndOfTheTurn_IfTheresAWinner(t *testing.T) {
+	currentPlayer := MockPlayer{"X", 4}
+	nextPlayer := MockPlayer{"O", 1}
+	game := CreateGame(MockUI{0}, EmptyBoard(), currentPlayer, nextPlayer, MockReferee{WinP1})
+	updatedGame := game.takeTurn()
+	assert.NotEqual(t, nextPlayer, updatedGame.players[0])
+}
+
+func TestGame_DoesNotSwitchThePlayersAtTheEndOfTheTurn_IfTheresATie(t *testing.T) {
+	currentPlayer := MockPlayer{"X", 4}
+	nextPlayer := MockPlayer{"O", 1}
+	game := CreateGame(MockUI{0}, EmptyBoard(), currentPlayer, nextPlayer, MockReferee{Tie})
+	updatedGame := game.takeTurn()
+	assert.NotEqual(t, nextPlayer, updatedGame.players[0])
+}
+
+func TestGame_EndsIfTheStatusIsWin(t *testing.T) {
+	currentPlayer := MockPlayer{"X", 4}
+	nextPlayer := MockPlayer{"O", 1}
+	game := CreateGame(MockUI{0}, EmptyBoard(), currentPlayer, nextPlayer, MockReferee{Tie})
+	updatedGame := game.takeTurn()
+	assert.NotEqual(t, nextPlayer, updatedGame.players[0])
 }
 
 func TestGame_PlaysWithDifferentMarkersEachTurn(t *testing.T) {
 	currentPlayer := MockPlayer{"X", 4}
 	nextPlayer := MockPlayer{"O", 1}
-	game := Game{MockUI{0}, EmptyBoard(), []Player{currentPlayer, nextPlayer}}
+	game := CreateGame(MockUI{0}, EmptyBoard(), currentPlayer, nextPlayer, MockReferee{Continue})
 
 	updatedGame := game.takeTurn()
 	updatedGame = game.takeTurn()
@@ -51,10 +75,37 @@ func TestGame_CanCreateAGameWithTheGivenAttributes(t *testing.T) {
 	firstPlayer := MockPlayer{"X", 5}
 	secondPlayer := MockPlayer{"O", 2}
 
-	game := CreateGame(MockUI{1}, board, firstPlayer, secondPlayer)
+	game := CreateGame(MockUI{1}, board, firstPlayer, secondPlayer, MockReferee{Continue})
 
 	assert.Equal(t, board, game.board)
 	assert.Equal(t, firstPlayer, game.players[0])
 	assert.Equal(t, secondPlayer, game.players[1])
+}
 
+func TestGame_CanGetTheCurrentPlayersMarker_ForX(t *testing.T) {
+	board := Board{
+		E, E, E,
+		E, E, E,
+		E, E, E,
+	}
+	firstPlayer := MockPlayer{"X", 5}
+	secondPlayer := MockPlayer{"O", 2}
+
+	game := CreateGame(MockUI{1}, board, firstPlayer, secondPlayer, MockReferee{Continue})
+
+	assert.Equal(t, X, game.getCurrentPlayerMarker())
+}
+
+func TestGame_CanGetTheCurrentPlayersMarker_ForO(t *testing.T) {
+	board := Board{
+		E, E, E,
+		E, E, X,
+		E, E, E,
+	}
+	firstPlayer := MockPlayer{"X", 5}
+	secondPlayer := MockPlayer{"O", 2}
+
+	game := CreateGame(MockUI{1}, board, firstPlayer, secondPlayer, MockReferee{Continue})
+
+	assert.Equal(t, O, game.getCurrentPlayerMarker())
 }
--- game/mock_referee.go
+++ game/mock_referee.go
@@ -3,9 +3,9 @@ package game
 import . "github.com/go-number-1-fan/tic-tac-toe/board"
 
 type MockReferee struct {
-	mockGameStatus GameStatus
+	MockGameStatus GameStatus
 }
 
 func (referee MockReferee) GetGameStatus(board Board) GameStatus {
-	return referee.mockGameStatus
+	return referee.MockGameStatus
 }
