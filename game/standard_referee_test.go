5062010c95335f265c7eef68b33b0842c4ced711
Tom McGee <gonumber1fan@gmail.com>
2016-11-30 15:40:29 -0600

    Add diagonal win to StandardReferee

--- game/standard_referee_test.go
+++ game/standard_referee_test.go
@@ -171,7 +171,78 @@ func TestReferee_ReturnsWinP1_IfXWinsVertically_InADifferentRow(t *testing.T) {
 	assert.Equal(t, WinP1, ref.GetGameStatus(board))
 }
 
-func TestReferee_GetsAllTheVerticalAndHorizontalWinningCombinations(t *testing.T) {
+func TestReferee_GetAllTheDiagonalWinsForAnEmptyBoard(t *testing.T) {
+	board := EmptyBoard()
+	expectedWins := []WinableCombination{
+		WinableCombination{E, E, E},
+		WinableCombination{E, E, E},
+	}
+
+	assert.Equal(t, expectedWins, getDiagonalWins(board))
+}
+
+func TestReferee_GetAllTheDiagonalWinsForABoard(t *testing.T) {
+	board := Board{
+		E, E, X,
+		E, E, X,
+		E, E, X,
+	}
+	expectedWins := []WinableCombination{
+		WinableCombination{E, E, X},
+		WinableCombination{X, E, E},
+	}
+
+	assert.Equal(t, expectedWins, getDiagonalWins(board))
+}
+
+func TestReferee_GetAllTheDiagonalWinsForAFullBoard(t *testing.T) {
+	board := Board{
+		O, E, X,
+		E, X, X,
+		E, E, X,
+	}
+	expectedWins := []WinableCombination{
+		WinableCombination{O, X, X},
+		WinableCombination{X, X, E},
+	}
+
+	assert.Equal(t, expectedWins, getDiagonalWins(board))
+}
+
+func TestReferee_ReturnsP1WinsIfXWinsDiagonally(t *testing.T) {
+	ref := StandardReferee{}
+	board := Board{
+		X, E, O,
+		E, X, X,
+		E, E, X,
+	}
+
+	assert.Equal(t, WinP1, ref.GetGameStatus(board))
+}
+
+func TestReferee_ReturnsP1WinsIfOWinsDiagonally(t *testing.T) {
+	ref := StandardReferee{}
+	board := Board{
+		X, E, O,
+		E, O, X,
+		O, E, X,
+	}
+
+	assert.Equal(t, WinP2, ref.GetGameStatus(board))
+}
+
+func TestReferee_ReturnsContinueIfNoWin(t *testing.T) {
+	ref := StandardReferee{}
+	board := Board{
+		X, E, O,
+		E, E, X,
+		O, E, X,
+	}
+
+	assert.Equal(t, Continue, ref.GetGameStatus(board))
+}
+
+func TestReferee_GetsAllWinningCombinations(t *testing.T) {
 	board := Board{
 		X, O, E,
 		E, X, O,
@@ -185,6 +256,8 @@ func TestReferee_GetsAllTheVerticalAndHorizontalWinningCombinations(t *testing.T
 		WinableCombination{X, E, O},
 		WinableCombination{O, X, E},
 		WinableCombination{E, O, X},
+		WinableCombination{X, X, X},
+		WinableCombination{E, X, O},
 	}
 
 	assert.Equal(t, expectedWins, getAllWinableCombinations(board))
