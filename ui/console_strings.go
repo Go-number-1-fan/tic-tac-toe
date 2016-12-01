051dbf2f6cdc1f31d3ccde4518a3ada367484137
Tom McGee <gonumber1fan@gmail.com>
2016-12-01 09:14:15 -0600

    Add Computer Thinking Message

--- ui/console_strings.go
+++ ui/console_strings.go
@@ -1,12 +1,13 @@
 package ui
 
 const (
-	WelcomeMessage      string = "\n\n\nWelcome To Tic Tac Toe!\nFollow the instructions and enter requested information when prompted!\n\nMost Importantly... Have fun!"
-	TieMessage          string = "Game Over!\nIt's a tie!!\n"
-	WinMessage          string = " Wins!!\n"
-	SelectASpotMessage  string = ", Please select an open spot of the board by the index:\n"
-	MoveNotValidMessage string = "Your selection is Invalid!!\n"
-	NotANumberMessage   string = "You must enter a NUMBER: "
+	WelcomeMessage          string = "\n\n\nWelcome To Tic Tac Toe!\nFollow the instructions and enter requested information when prompted!\n\nMost Importantly... Have fun!"
+	ComputerThinkingMessage string = "The Computer is Thinking...\n"
+	TieMessage              string = "Game Over!\nIt's a tie!!\n"
+	WinMessage              string = " Wins!!\n"
+	SelectASpotMessage      string = ", Please select an open spot of the board by the index:\n"
+	MoveNotValidMessage     string = "Your selection is Invalid!!\n"
+	NotANumberMessage       string = "You must enter a NUMBER: "
 
 	NewLineString           string = "\n"
 	HorizontalDividerString string = "-"
