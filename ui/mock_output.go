19fa630699eb0dc8c59b44b81d86369d8cd4a57a
Tom McGee <gonumber1fan@gmail.com>
2016-12-02 11:18:43 -0600

    Clear screens before DisplayBoard and Options

--- ui/mock_output.go
+++ ui/mock_output.go
@@ -1,7 +1,11 @@
 package ui
 
+import "fmt"
+
 type MockOutput struct{}
 
-func (output MockOutput) Write(str string) {
+func (output MockOutput) Write(str string) {}
 
+func (output MockOutput) Clear() {
+	fmt.Println("CLEARED")
 }
